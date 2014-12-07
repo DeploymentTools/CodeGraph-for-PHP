e610aaac06040c32f8f32cccfd140d1da6b8b01d
Bogdan Anton <contact@bogdananton.ro>
2014-12-07 19:01:43 +0200

    tests

--- tests/starter.py
+++ tests/starter.py
@@ -5,33 +5,40 @@ import sys
 sys.path.append('src')
 from FunctionBodyExtractor import FunctionBodyExtractor
 
+# TODO  results aren't returned in the same order all the time. fix this in extract()
 logging.basicConfig(level=logging.DEBUG)
 
 class TestBA(unittest.TestCase):
-    def test_basic_extractor(self):
-        expected = {
-            'arrays': ['$this->db'],
-            'generic': ['$this->counter', '$this->db'],
-            'methods': ['$this->run', 'Registry::getInstance']
-        }
+    fbe = False
 
-        fbe = FunctionBodyExtractor()
+    def setUp(self):
+        self.fbe = FunctionBodyExtractor()
 
-        fbe.body = """
+        self.fbe.body = """
         $this->run();
         $this->counter++;
 
         $this->db = Registry::getInstance('DB');
         $this->db[0] = null;
 """
-        response = fbe.extract()
 
-        # TODO  results aren't returned in the same order all the time. fix this in extract()
-        # self.assertEqual(expected, response)
+    def test_arrays_extracted(self):
+        response = self.fbe.extract()
+        response['arrays'].sort() # temp
 
-        self.assertEqual(expected['arrays'].sort(), response['arrays'].sort())
-        self.assertEqual(expected['generic'].sort(), response['generic'].sort())
-        self.assertEqual(expected['methods'].sort(), response['methods'].sort())
+        self.assertEqual(response['arrays'], ['$this->db'])
+
+    def test_generic_extracted(self):
+        response = self.fbe.extract()
+        response['generic'].sort() # temp
+        
+        self.assertEqual(response['generic'], ['$this->counter', '$this->db'])
+
+    def test_methods_extracted(self):
+        response = self.fbe.extract()
+        response['methods'].sort() # temp
+        
+        self.assertEqual(response['methods'], ['$this->run', 'Registry::getInstance'])
 
 if __name__ == '__main__':
     unittest.main()
