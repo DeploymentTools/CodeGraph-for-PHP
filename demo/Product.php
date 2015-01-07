8dcd98f1eb2215e45786bc2556b25f232923ca18
Bogdan Anton <contact@bogdananton.ro>
2015-01-07 22:47:09 +0200

    Added documentation for Product class example
    refs #2

--- demo/Product.php
+++ demo/Product.php
@@ -1,10 +1,29 @@
 <?php
 
+/**
+ * Product object.
+ */
 class Product
 {
+	/**
+	 * Product code
+	 * @var string
+	 */
 	public $Code;
+
+	/**
+	 * Quantity
+	 * @var integer
+	 */
 	public $Quantity;
 
+	/**
+	 * Stores the code and quantity.
+	 * The quantity will be cast to integer.
+	 * 
+	 * @param string
+	 * @param integer
+	 */
 	public function __construct($ProductCode = "", $Quantity = 1)
 	{
 		$this->Code = $ProductCode;
