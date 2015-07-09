2f494772af81d6d3ddbf397590b2e202f988df17
Bogdan Anton <contact@bogdananton.ro>
2015-07-10 02:43:37 +0300

    Method contents analysis [1].

--- src/FunctionBodyExtractor.py
+++ src/FunctionBodyExtractor.py
@@ -21,10 +21,155 @@
 # SOFTWARE.
 
 import re
+import os
 
 class FunctionBodyExtractor():
     signature = ""
     body = ""
+
+    phrase_transient_content = ""
+    phrase_content = ""
+    phrases_list = []
+    current_phrase = {}
+
+    # def get_last_thought(self):
+    #     nr_thoughts = self.get_nr_thoughts();
+
+    #     if nr_thoughts == 0:
+    #         return None
+
+    #     return self.current_phrase['thinking_about'][nr_thoughts - 1]
+
+    # def get_nr_thoughts(self):
+    #     return len(self.current_phrase['thinking_about'])
+
+    # def think_about(self, message):
+    #     thought = message
+
+    #     existing_thoughts_nr = self.get_nr_thoughts()
+
+    #     if ((existing_thoughts_nr > 0) and (self.get_last_thought() != thought)) or (existing_thoughts_nr == 0):
+    #         self.current_phrase['thinking_about'].append(thought)
+
+    in_single_quotes = False
+    in_double_quotes = False
+    in_single_comment = False
+    in_multi_line_comment = False
+    is_end_of_phrase = False
+
+    def get_char(self, content, offset_end):
+        char = None
+
+        if content and isinstance(content, str) and len(content) > 0:
+            size_content = len(content)
+            if offset_end < 0:
+                if offset_end + size_content >= 0:
+                    char = content[size_content + offset_end]
+                
+            elif offset_end < size_content:
+                char = content[offset_end]
+
+        return char
+
+    def analyse_flags(self):
+        current_phrase_content = self.phrase_content
+
+        current_char = self.get_char(current_phrase_content, -1)
+        previous_char = self.get_char(current_phrase_content, -2)
+
+        # print('---->' + current_phrase_content + '<----')
+
+        if not self.in_single_comment and not self.in_multi_line_comment:
+            if (current_char == "'") and (previous_char != '\\') and (self.in_double_quotes is False):
+                self.in_single_quotes = not self.in_single_quotes
+
+            if (current_char == '"') and (previous_char != '\\') and (self.in_single_quotes is False):
+                self.in_double_quotes = not self.in_double_quotes
+
+        if not self.in_single_quotes and not self.in_double_quotes:
+            if not self.in_multi_line_comment and (current_char == "/") and (previous_char == '/') and not self.in_single_comment:
+                self.in_single_comment = True
+                # store current phrase, to resume after exiting from line comment. Strip the single line comment "//" chars
+                self.phrase_transient_content = self.phrase_content[0:-2]
+                # wipe current phrase, will be restored after phrase end
+                self.phrase_content = ''
+                # print('begin single comment')
+
+            if (current_char == "*") and (previous_char == '/') and (not self.in_multi_line_comment) and (not self.in_single_comment):
+                self.in_multi_line_comment = True
+                # self.phrase_transient_content = self.phrase_content
+                # print('begin multiline')
+
+            if (current_char == "/") and (previous_char == '*') and (self.in_multi_line_comment is True):
+                self.in_multi_line_comment = False
+                # self.phrase_transient_content = self.phrase_content
+                self.phrase_content = ''
+                # print('end multiline')
+
+            if ((current_char == os.linesep) or (current_char == "\n") or (current_char == "\r\n")) and self.in_single_comment:
+                self.in_single_comment = False
+                # print('end single comment')
+
+                # if not self.in_multi_line_comment:
+                self.phrase_content = self.phrase_transient_content
+                # print('reset phrase content')
+
+        if (current_char == ';') and not self.in_single_comment and not self.in_multi_line_comment and not self.in_single_quotes and not self.in_double_quotes:
+            self.is_end_of_phrase = True
+
+    def analise_phrase(self):
+        self.analyse_flags()
+
+        if self.is_end_of_phrase:
+            self.phrases_list.append(self.phrase_content)
+            self.phrase_content = ''
+            self.is_end_of_phrase = False
+
+        # startsWithVariable = re.findall(r'^(\$[\w\d]+)$', current_phrase_content)
+        # startsWithDollar = re.findall(r'^(\$)$', current_phrase_content)
+
+        # if (startsWithDollar):
+        #     self.think_about('expecting_closure_or_variable')
+
+        # elif (startsWithVariable):
+        #     self.current_phrase['action'] = 'variable_prepare' # probably will set
+        #     self.current_phrase['subject'] = startsWithVariable
+        #     self.think_about('building_variable')
+    
+        # elif (self.current_phrase['action'] == 'variable_prepare') and (current_char == '='):
+        #     self.current_phrase['action'] = 'variable_set'
+        #     self.think_about('waiting_for_variable_set')
+        #     self.current_char.entities.append()
+
+
+        # if (current_char == '"') or (current_char == "'"):
+        #     if (self.get_last_thought() == 'listening_for_string'):
+        #         self.think_about('stop_listening_for_string')
+
+        # print([self.phrase_content, self.current_phrase])
+
+    def appendChar(self, x):
+        if ((self.phrase_content == "") and (x.strip() == "")) == False:
+            self.phrase_content += x
+            self.analise_phrase()
+
+    def reset(self):
+        self.current_phrase['type'] = False
+        self.current_phrase['action'] = False
+        self.current_phrase['subject'] = False
+        self.current_phrase['thinking_about'] = []
+        self.current_phrase['entities'] = []
+
+    # will split the method into "phrases"
+    def phrases(self):
+        self.reset()
+
+        for x in self.body:
+            # if x.strip() != "":
+            #     print(x)
+            #
+            self.appendChar(x)
+            pass
 
     def extract(self):
         # WORD_reservedPHP = ['array', 'declare', 'die', 'class', 'echo', 'elseif', 'empty', 'eval', 'exit', 'for', 'foreach', 'global', 'if', 'include', 'include_once', 'isset', 'list', 'print', 'require', 'require_once', 'return', 'switch', 'unset', 'while', 'catch', 'or', 'and', 'xor']
@@ -34,34 +179,34 @@ class FunctionBodyExtractor():
         textBody = self.body
 
         re.sub(r'\breturn\b', '', textBody)
-        textBody = textBody.replace(" as ", "|as|").replace(" ", "").replace("\t", "")
+        textBody = textBody.replace(" as ", "|as|").replace("\t", "")
         response = {}
 
-        all_variables = re.findall(r'([a-zA-Z0-9_\:\-\>\$\(\)]+)\(', textBody)
+        all_variables = re.findall(r'([a-zA-Z0-9\s_\:\-\>\$\(\)]+)\(', textBody)
         methods = []
         for variable in all_variables:
             if (('->' in variable) or ('::' in variable)):
-                methods.append(variable)
+                methods.append(variable.strip())
             pass
         response['methods'] = list(set(methods))
 
-        all_variables = re.findall(r'([a-zA-Z0-9_\:\-\>]+)\[', textBody)
+        all_variables = re.findall(r'([a-zA-Z0-9\s_\:\-\>]+)\[', textBody)
         arrays = []
         for variable in all_variables:
             if (('->' in variable) or ('::' in variable)):
-                arrays.append('$' + variable)
+                arrays.append('$' + variable.strip())
             pass
         response['arrays'] = list(set(arrays))
 
-        all_variables = re.findall(r'\$([a-zA-Z0-9_\:\-\>]+)', textBody)
+        all_variables = re.findall(r'\$([a-zA-Z0-9\s_\:\-\>]+)', textBody)
         class_attributes = []
         for variable in all_variables:
             if (('->' in variable) or ('::' in variable)):
                 try:
-                    response['methods'].index('$' + variable)
+                    response['methods'].index('$' + variable.strip())
                     pass
                 except ValueError:
-                    class_attributes.append('$' + variable)
+                    class_attributes.append('$' + variable.strip())
                     pass
             pass
 
--- tests/starter.py
+++ tests/starter.py
@@ -1,37 +1,280 @@
 import logging
 import unittest
 
-import sys
-sys.path.append('src')
-from FunctionBodyExtractor import FunctionBodyExtractor
-
 logging.basicConfig(level=logging.DEBUG)
 
-class TestBA(unittest.TestCase):
-    fbe = False
+import sys
+sys.path.append('src')
+
+from FunctionBodyExtractor import FunctionBodyExtractor
+
+class TestGetChar(unittest.TestCase):
+
+    def test_when_last_char_is_string_then_return_the_char(self):
+        fbe = FunctionBodyExtractor()
+
+        response = fbe.get_char('content123', -1)
+        self.assertEqual('3', response)
+
+    def test_when_last_char_is_space_then_return_the_empty_space_char(self):
+        fbe = FunctionBodyExtractor()
+
+        response = fbe.get_char('content123 ', -1)
+        self.assertEqual(' ', response)
+
+    def test_when_input_string_is_empty_then_return_None(self):
+        fbe = FunctionBodyExtractor()
+
+        response = fbe.get_char('', -1)
+        self.assertEqual(None, response)
+
+    def test_when_input_is_None_then_return_None(self):
+        fbe = FunctionBodyExtractor()
+
+        response = fbe.get_char(None, -1)
+        self.assertEqual(None, response)
+
+    def test_when_input_is_string_and_offset_is_zero_then_get_first_char(self):
+        fbe = FunctionBodyExtractor()
+
+        response = fbe.get_char('abcdef', 0)
+        self.assertEqual('a', response)
+
+    def test_when_input_is_string_and_offset_is_positive_then_get_char(self):
+        fbe = FunctionBodyExtractor()
+
+        response = fbe.get_char('ABCDEF', 2)
+        self.assertEqual('C', response)
+
+    def test_when_input_is_string_and_offset_is_greater_than_string_length_then_return_None(self):
+        fbe = FunctionBodyExtractor()
+
+        response = fbe.get_char('ABCDEF', 10)
+        self.assertEqual(None, response)
+
+
+class TestAnaliseFlags(unittest.TestCase):
+    fbe = None
 
     def setUp(self):
         self.fbe = FunctionBodyExtractor()
 
-        self.fbe.body = """
-        $this->run();
-        $this->counter++;
+    def assert_flags(self, expect_in_single_quotes, expect_in_double_quotes, expect_in_single_comment, expect_in_multi_line_comment):
+        self.assertEqual(expect_in_single_quotes, self.fbe.in_single_quotes)
+        self.assertEqual(expect_in_double_quotes, self.fbe.in_double_quotes)
+        self.assertEqual(expect_in_single_comment, self.fbe.in_single_comment)
+        self.assertEqual(expect_in_multi_line_comment, self.fbe.in_multi_line_comment)
 
-        $this->db = Registry::getInstance('DB');
-        $this->db[0] = null;
+    def test_when_string_opened_single_quotes_then_change_single_quote_to_true_if_all_other_flags_are_false(self):
+        self.fbe.phrase_content = "$a = '"
+        self.fbe.analyse_flags()
+        self.assert_flags(True, False, False, False)
+
+    def test_when_string_opened_double_quotes_then_change_double_quote_to_true_if_all_other_flags_are_false(self):
+        self.fbe.phrase_content = '$a = "'
+        self.fbe.analyse_flags()
+        self.assert_flags(False, True, False, False)
+
+    def test_when_string_opened_single_quotes_and_is_in_single_line_comment_then_dont_change_single_quote_flag(self):
+        self.fbe.in_single_comment = True
+        self.fbe.phrase_content = '$a = "'
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, True, False)
+
+    def test_when_string_opened_single_quotes_and_is_in_multiline_comment_then_dont_change_single_quote_flag(self):
+        self.fbe.in_multi_line_comment = True
+        self.fbe.phrase_content = '$a = "'
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, True)
+
+    def test_when_not_in_quotes_and_is_start_of_single_line_comment_then_change_single_line_flag(self):
+        self.fbe.phrase_content = '$a = 1; //'
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, True, False)
+
+    def test_when_in_quotes_and_is_start_of_single_line_comment_then_dont_change_single_line_flag(self):
+        # single quotes
+        self.fbe.in_single_quotes = True
+
+        self.fbe.phrase_content = '$a = 1; //'
+        self.fbe.analyse_flags()
+
+        self.fbe.in_single_quotes = False # reset
+        self.assert_flags(False, False, False, False)
+
+        # double quotes
+        self.fbe.in_double_quotes = True
+
+        self.fbe.phrase_content = '$a = 1; //'
+        self.fbe.analyse_flags()
+
+        self.fbe.in_double_quotes = False # reset
+        self.assert_flags(False, False, False, False)
+
+    def test_when_not_in_quotes_and_is_start_of_multi_line_comment_then_change_multi_line_flag(self):
+        self.fbe.phrase_content = '$a = 1; /*'
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, True)
+
+    def test_when_in_quotes_and_is_start_of_multi_line_comment_then_dont_change_multi_line_flag(self):
+        # single quotes
+        self.fbe.in_double_quotes = True
+        self.fbe.phrase_content = '$a = 1; /*'
+        self.fbe.analyse_flags()
+        self.fbe.in_double_quotes = False
+        self.assert_flags(False, False, False, False)
+
+        # double quotes
+        self.fbe.in_single_quotes = True
+        self.fbe.phrase_content = '$a = 1; /*'
+        self.fbe.analyse_flags()
+        self.fbe.in_single_quotes = False
+        self.assert_flags(False, False, False, False)
+
+    def test_when_in_single_line_comment_and_is_multi_line_comment_then_change_multi_line_flag(self):
+        self.fbe.phrase_content = "$a = 1; //"
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, True, False)
+
+        self.fbe.phrase_content = """$a = 1; //
 """
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, False)
 
-    def test_arrays_extracted(self):
-        response = self.fbe.extract()
-        self.assertEqual(response['arrays'], ['$this->db'])
+        self.fbe.phrase_content = """$a = 1; //
+/"""
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, False)
 
-    def test_generic_extracted(self):
-        response = self.fbe.extract()
-        self.assertEqual(response['generic'], ['$this->counter', '$this->db'])
+        self.fbe.phrase_content = """$a = 1; //
+/*"""
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, True)
 
-    def test_methods_extracted(self):
-        response = self.fbe.extract()
-        self.assertEqual(response['methods'], ['$this->run', 'Registry::getInstance'])
+        self.fbe.phrase_content = """$a = 1; //
+/*a"""
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, True)
+
+        self.fbe.phrase_content = """$a = 1; //
+/*a*"""
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, True)
+
+        self.fbe.phrase_content = """$a = 1; //
+/*a*/"""
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, False)
+
+    def test_when_latest_char_is_end_of_line_then_reset_single_comment_flag(self):
+        self.fbe.in_single_comment = True
+        self.fbe.phrase_content = """$a = 1;
+"""
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, False)
+
+        self.fbe.in_single_comment = True
+        self.fbe.phrase_content = """$a = 1;\n"""
+        self.fbe.analyse_flags()
+        self.assert_flags(False, False, False, False)
+
+
+class TestPhrases(unittest.TestCase):
+    fbe = None
+
+    def setUp(self):
+        self.fbe = FunctionBodyExtractor()
+
+    def test_get_simple_phrases(self):
+        self.fbe.body = """
+            $a = 'abc';
+            // $b = '123';
+
+            /*
+            $c = 'DEF';
+            */
+
+            $d = new \stdClass;
+        """
+
+        self.fbe.phrases()
+        self.assertEqual(["$a = 'abc';", "$d = new \stdClass;"], self.fbe.phrases_list)
+
+    @unittest.skip("extract blocks as phrases/stories")
+    def test_get_if_statement_phrase(self):
+
+        self.fbe.body = """
+            static $statement = false;
+
+            if (isset($content0)) {
+                $a = 2;
+                die(0);
+            }
+
+            if (isset($content1)) { // explain
+                // if something then exit
+                exit();
+            }
+
+            if (isset($content2) /* && $isDebug */) {
+                die(2);
+            }
+
+            return '123';
+"""
+        self.fbe.phrases()
+
+        expected = []
+        expected.append("static $statement = false;")
+
+        self.assertEqual(expected, self.fbe.phrases_list)
+
+
+#        self.fbe.body = """
+#        $this->run();
+#        $this->counter++;
+#
+#        $this->db = Registry::getInstance('DB');
+#        $this->db[0] = null;
+#"""
+#
+#     def test_arrays_extracted(self):
+#         response = self.fbe.extract()
+#         self.assertEqual(response['arrays'], ['$this->db'])
+#
+#     def test_generic_extracted(self):
+#         response = self.fbe.extract()
+#         self.assertEqual(response['generic'], ['$this->counter', '$this->db'])
+#
+#     def test_get_static_class_method(self):
+#         self.fbe = FunctionBodyExtractor()
+#
+#         self.fbe.body = """
+#         $this->db = Registry::getInstance('DB');
+# """
+#         response = self.fbe.extract()
+#         self.assertEqual(response['methods'], ['Registry::getInstance'])
+#
+#     def test_get_static_object_method(self):
+#         self.fbe = FunctionBodyExtractor()
+#
+#         self.fbe.body = """
+#         $s = new \Core;
+#         $this->db = $s::getInstance('DB');
+# """
+#         response = self.fbe.extract()
+#         self.assertEqual(response['methods'], ['$s::getInstance'])
+#
+#     # note: check
+#     def test_get_static_container_method(self):
+#         self.fbe = FunctionBodyExtractor()
+#
+#         self.fbe.body = """
+#         $s = new \Core;
+#         $this->db = $s[$databaseEngine]::getInstance('DB');
+# """
+#         response = self.fbe.extract()
+#         self.assertEqual(response['methods'], ['$s[$databaseEngine]::getInstance'])
 
 if __name__ == '__main__':
     unittest.main()
